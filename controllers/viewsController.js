697a57b77f3df00a1b0be9e6520afca37d5c1f98
Sarib Siddiqui <cold.sarib@gmail.com>
2025-05-08 18:51:14 +0500

    Add authentication features: implement cookie-based JWT handling, create login form and functionality, and update view routing for protected tour access.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -7,7 +7,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
@@ -15,3 +15,9 @@ exports.getTour = catchAsync(async (req, res) => {
 
   res.status(200).render('tour', { title: `${tour.name} Tour`, tour });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
