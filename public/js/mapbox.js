7c610b0b282f6d57bfe312fb43a93f77101d7875
Sarib Siddiqui <cold.sarib@gmail.com>
2025-05-08 16:48:24 +0500

    Enhance tour and overview views with detailed information and dynamic content rendering; update routing for tour details and fix asset paths in Pug templates.

---
+++ public/js/mapbox.js
@@ -0,0 +1,44 @@
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2FyaWItMSIsImEiOiJjbWFmOHVqbW4wMDZjMmlxd3g2cGJ5bmc2In0.nu57P-BBjtLwqOm2Y-wEzw';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  scrollZoom: false
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 4,
+  //   interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    right: 100,
+    left: 100
+  }
+});
